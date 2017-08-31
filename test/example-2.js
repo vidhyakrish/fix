38382701652a2d00ffa1b8e06579eb4a5819fea3
vidhyakrish <vidhyaakrish@gmail.com>
2017-08-31 14:49:07 +0100

    Update example-2.js

--- test/example-2.js
+++ test/example-2.js
@@ -1,20 +1,24 @@
-// dependencies:
+ // dependencies:
 const expect = require('chai').expect;
 
  // implement this:
- function b(x){
-   // return "b"+ x;
-   //  return (x) =>  "bo" + x;
-  () =>  (x) => return "boo" + x;
+
+ function b(lastLetter, prefix = "b") {
+  if (lastLetter) {
+    //if we're ending the chain, return everything so far with the last letter on the end
+    return prefix + lastLetter;
+  }
+  //if not, then return   this function with a slightly longer prefix
+  return lastLetter => b(lastLetter, prefix + "o");
 }
 // unit tests:
 describe("implement function b", function() {
-    it("SHOULD work for the following cases", function() {
-      console.log(b()("m"));
-        // expect(b("m")).to.equal("bm");
-        // expect(b()("m")).to.equal("bom");
-        expect(b()()("m")).to.equal("boom");
-        // expect(b()()()("m")).to.equal("booom");
-        // expect(b()()()()("t")).to.equal("boooot");
-    });
+  it("SHOULD work for the following cases", function() {
+    console.log(b()("m"));
+    expect(b("m")).to.equal("bm");
+    expect(b()("m")).to.equal("bom");
+    expect(b()()("m")).to.equal("boom");
+    expect(b()()()("m")).to.equal("booom");
+    expect(b()()()()("t")).to.equal("boooot");
+  });
 });
